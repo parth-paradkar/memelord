8c55b26ed3724617d3698f598b4502a7a56f7757
Parth Paradkar <parthparadkar3@gmail.com>
2020-04-06 18:14:02 +0530

    Loop through subreddits

--- reddit/bot.py
+++ reddit/bot.py
@@ -5,14 +5,13 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 import os
 
-post = list(reddit.subreddit('ProgrammerHumor').hot(limit=5))[0].url
+headers = { 'Content-type': 'application/json' }
 
-headers = {
-            'Content-type': 'application/json'
-        }
-
-data = {
-            'text': post
-        }
-
-response = requests.post(os.getenv("SLACK_WEBHOOK"), headers=headers, data=json.dumps(data)) 
+with open('subreddits.json', 'r') as f:
+    data = json.load(f)
+    subreddits = data['subreddits']
+    for sub in subreddits:
+        post = list(reddit.subreddit(sub).hot(limit=5))[0]
+        if(post.url.endswith('.png') or post.url.endswith('.jpg')):
+            data = { 'text': f'r/{sub} {post.title}\n{post.url}' }
+            response = requests.post(os.getenv("SLACK_WEBHOOK"), headers=headers, data=json.dumps(data))

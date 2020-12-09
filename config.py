440e1c9374782176b2e25d874f411d49d0d0aca9
Nitya <53599318+nitya123-github@users.noreply.github.com>
2020-12-09 15:02:18 +0530

    updated config.py

--- config.py
+++ config.py
@@ -1,48 +1,22 @@
-#twitter- config.py
+# Replace the values in the fields below with your own info
 
-import json
-import os
-import tweepy
-import mysql.connector as mysql
-
-from dotenv import (load_dotenv, find_dotenv)
-
-load_dotenv(find_dotenv())
-
+# twitter
 user = os.getenv("MYSQL_USER")
 password = os.getenv("MYSQL_PASSWORD")
 host = os.getenv("MYSQL_HOST")
 database = os.getenv("MYSQL_DATABASE")
-
-
 consumer_key = os.getenv("TWITTER_CONSUMER_KEY")
 consumer_secret = os.getenv("TWITTER_CONSUMER_SECRET_KEY")
-
-auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
-api = tweepy.API(auth)
-
-db = mysql.connect(user=user, passwd=password, host=host, database=database)
-
-with open("twitter/twitter-accounts.json") as f:
-    data = json.load(f)
-    twitterAccounts = data['twitterAccounts']
-
 SLACK_WEBHOOK = os.getenv("SLACK_WEBHOOK")
 
+#imageupload
+client_id = os.getenv("IMGUR_CLIENT_ID")
+imgur_upload_url = "https://api.imgur.com/3/image"
 
-#REDDIT-setup.py
-
-import praw
-import json
-from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv())
-import os
-
-
-reddit = praw.Reddit(user_agent=os.getenv("REDDIT_USER_AGENT"),
-                  client_id=os.getenv("REDDIT_CLIENT_ID"),
-                  client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
-                  username=os.getenv("REDDIT_USERNAME"),
-                  password=os.getenv("REDDIT_PASSWORD")
-                  )
-
+#Reddit
+REDDIT_USER_AGENT = os.getenv("REDDIT_USER_AGENT")
+REDDIT_CLIENT_ID = os.getenv("REDDIT_CLIENT_ID")
+REDDIT_CLIENT_SECRET = os.getenv("REDDIT_CLIENT_SECRET")
+REDDIT_USERNAME = os.getenv("REDDIT_USERNAME")
+REDDIT_PASSWORD = os.getenv("REDDIT_PASSWORD")
+SLACK_WEBHOOK = os.getenv("SLACK_WEBHOOK")

6886de63b6626e8cdf6a59da35b35ba4a6cf3624
Nitya <53599318+nitya123-github@users.noreply.github.com>
2020-12-10 21:30:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
-# Replace the values in the fields below with your own info
+import json
+import os
+import tweepy
+import mysql.connector as mysql
+from dotenv import (load_dotenv, find_dotenv)
+load_dotenv(find_dotenv())
 
+# Replace the values in the fields below with your own info
 #[DATABASE]
 user = os.getenv("MYSQL_USER")
 password = os.getenv("MYSQL_PASSWORD")
@@ -9,6 +15,12 @@ database = os.getenv("MYSQL_DATABASE")
 #[TWITTER]
 consumer_key = os.getenv("TWITTER_CONSUMER_KEY")
 consumer_secret = os.getenv("TWITTER_CONSUMER_SECRET_KEY")
+auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
+api = tweepy.API(auth)
+db = mysql.connect(user=user, passwd=password, host=host, database=database)
+with open("twitter/twitter-accounts.json") as f:
+    data = json.load(f)
+    twitterAccounts = data['twitterAccounts']
 
 #[BOT]
 SLACK_WEBHOOK = os.getenv("SLACK_WEBHOOK")

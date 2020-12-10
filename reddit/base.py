7859cbc09b59dc686388e356155540d9681b5a69
Nitya <53599318+nitya123-github@users.noreply.github.com>
2020-12-10 15:09:26 +0530

    Update base.py

--- reddit/base.py
+++ reddit/base.py
@@ -2,18 +2,14 @@ import praw
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 import os
+import config
 
-REDDIT_USER_AGENT = os.getenv("REDDIT_USER_AGENT")
-REDDIT_CLIENT_ID = os.getenv("REDDIT_CLIENT_ID")
-REDDIT_CLIENT_SECRET = os.getenv("REDDIT_CLIENT_SECRET")
-REDDIT_USERNAME = os.getenv("REDDIT_USERNAME")
-REDDIT_PASSWORD = os.getenv("REDDIT_PASSWORD")
-SLACK_WEBHOOK = os.getenv("SLACK_WEBHOOK")
 
-reddit = praw.Reddit(user_agent=REDDIT_USER_AGENT,
-                  client_id=REDDIT_CLIENT_ID,
-                  client_secret=REDDIT_CLIENT_SECRET,
-                  username=REDDIT_USERNAME,
-                  password=REDDIT_PASSWORD
+
+reddit = praw.Reddit(user_agent= config.REDDIT_USER_AGENT,
+                  client_id=config.REDDIT_CLIENT_ID,
+                  client_secret=config.REDDIT_CLIENT_SECRET,
+                  username=config.REDDIT_USERNAME,
+                  password=config.REDDIT_PASSWORD
                   )
 

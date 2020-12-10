9f16fb0a93867fe5453c630439af9fd6b9f7a8a7
Nitya <53599318+nitya123-github@users.noreply.github.com>
2020-12-10 21:23:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,23 @@
 # Replace the values in the fields below with your own info
 
-[DATABASE]
+#[DATABASE]
 user = os.getenv("MYSQL_USER")
 password = os.getenv("MYSQL_PASSWORD")
 host = os.getenv("MYSQL_HOST")
 database = os.getenv("MYSQL_DATABASE")
 
-[TWITTER]
+#[TWITTER]
 consumer_key = os.getenv("TWITTER_CONSUMER_KEY")
 consumer_secret = os.getenv("TWITTER_CONSUMER_SECRET_KEY")
 
-[BOT]
+#[BOT]
 SLACK_WEBHOOK = os.getenv("SLACK_WEBHOOK")
 
-[IMAGEUPLOAD]
+#[IMAGEUPLOAD]
 client_id = os.getenv("IMGUR_CLIENT_ID")
 imgur_upload_url = "https://api.imgur.com/3/image"
 
-[REDDIT]
+#[REDDIT]
 REDDIT_USER_AGENT = os.getenv("REDDIT_USER_AGENT")
 REDDIT_CLIENT_ID = os.getenv("REDDIT_CLIENT_ID")
 REDDIT_CLIENT_SECRET = os.getenv("REDDIT_CLIENT_SECRET")

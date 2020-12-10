b7c54e64ab7d434ce3799b66867b82081a158709
Nitya <53599318+nitya123-github@users.noreply.github.com>
2020-12-10 21:38:04 +0530

    Update base.py

--- reddit/base.py
+++ reddit/base.py
@@ -1,11 +1,4 @@
-import praw
-from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv())
-import os
 import config
-
-
-
 reddit = praw.Reddit(user_agent= config.REDDIT_USER_AGENT,
                   client_id=config.REDDIT_CLIENT_ID,
                   client_secret=config.REDDIT_CLIENT_SECRET,
